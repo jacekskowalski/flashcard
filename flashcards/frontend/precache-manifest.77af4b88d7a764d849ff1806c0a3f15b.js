446eccec465ede96f6501db9f7c9598d34b7703f
mlawinski <mlawinski@edu.cdv.pl>
2019-03-12 21:51:57 +0100

    front-end

---
+++ flashcards/frontend/precache-manifest.77af4b88d7a764d849ff1806c0a3f15b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "87f6ed101ea2d992ccf2",
+    "url": "/static/js/main.87f6ed10.chunk.js"
+  },
+  {
+    "revision": "ba35ec618ea0f9ae687b",
+    "url": "/static/js/2.ba35ec61.chunk.js"
+  },
+  {
+    "revision": "87f6ed101ea2d992ccf2",
+    "url": "/static/css/main.3e1d0b20.chunk.css"
+  },
+  {
+    "revision": "134a16702caf05b0579311c23a658e94",
+    "url": "/index.html"
+  }
+];
