28ec94eb5589e11bdfdbd271be9212d279ccb07b
mlawinski <mlawinski@edu.cdv.pl>
2019-03-28 23:16:27 +0100

    Stworzenie próbnej wersj logowania, rejestracji oraz przypomnienia hasła

---
+++ flashcards/frontend/precache-manifest.40eafd33ff6134ac41de0cab42f87a3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "83057837880c3a89c3bf",
+    "url": "/static/js/main.83057837.chunk.js"
+  },
+  {
+    "revision": "c54636f1a57a5d9f1a2e",
+    "url": "/static/js/2.c54636f1.chunk.js"
+  },
+  {
+    "revision": "83057837880c3a89c3bf",
+    "url": "/static/css/main.c1fe448c.chunk.css"
+  },
+  {
+    "revision": "0a1f704e339c808fae59dd25a946aca9",
+    "url": "/index.html"
+  }
+];
