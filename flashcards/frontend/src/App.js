13564f45dbf1ae1942df485d6fa1b089c63a98d9
jacekskowalski <jsk_profil@hotmail.com>
2019-05-26 07:05:51 +0200

    Modified frontend

--- flashcards/frontend/src/App.js
+++ flashcards/frontend/src/App.js
@@ -1,75 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class LoginForm extends Component {
-
-    handleSignIn(e) {
-        e.preventDefault()
-        let email = this.refs.email.value;
-        let pswd = this.refs.pswd.value;
-        this.props.onSignIn(email, pswd)
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSignIn.bind(this)}>
-                <h3>Sign in</h3>
-                <input type="text" ref="email" placeholder="username" />
-                <input type="password" ref="pswd" placeholder="password" />
-                <input type="submit" value="Login" />
-            </form>
-        )
-    }
-
-}
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import Footer from './Layouts/Footer/Footer'
+import Main from './Layouts/Main'
 
 
 class App extends Component {
 
-    constructor(props) {
-        super(props)
-        // the initial application state
-        this.state = {
-            user: null
-        }
-    }
+  
+  render() {
+    
+    console.log(this.props.keys)
+    
+    return (
+      <>
+      
 
-    // App "actions" (functions that modify state)
-    signIn(email, pswd) {
-        // This is where you would call Firebase, an API etc...
-        // calling setState will re-render the entire app (efficiently!)
-        this.setState({
-            user: {
-                email,
-                pswd,
-            }
-        })
-    }
+      <main>
+      <Main></Main>
+      </main>
 
-    signOut() {
-        // clear out user from state
-        this.setState({user: null})
-    }
 
-    render() {
-        // Here we pass relevant state to our child components
-        // as props. Note that functions are passed using `bind` to
-        // make sure we keep our scope to App
-        return (
-            <div>
-                <h1>My cool App</h1>
-                {
 
-                        <LoginForm
-                    onSignIn={this.signIn.bind(this)}
-                        />
-                }
-            </div>
-        )
 
-    }
-
+<footer><Footer></Footer></footer>
+      
+      
+      </>
+    );
+  }
 }
 
-export default  App
+export default App;
